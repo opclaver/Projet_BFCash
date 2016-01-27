761489261995c53c9f85f7b9e3b42d7db081c3db
paideur <toussaintpahima@gmail.com>
2016-01-27 22:31:11 +0100

    first commit welcome page

--- BFCash/index.php
+++ BFCash/index.php
@@ -28,14 +28,9 @@ require "Includes/header.php";
          else{
              $_SESSION['flash']['danger'] ="";
          }
-
-        ?>
-
-        <?php
         if(!empty($_SESSION['flash']['success'])){
 
             ?>
-
             <div class="alert alert-success">
                 <a href="index.php" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                 <?php
@@ -47,9 +42,46 @@ require "Includes/header.php";
         else{
             $_SESSION['flash']['success'] ="";
         }
-
         ?>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li style="background: red" data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li style="background: red" data-target="#myCarousel" data-slide-to="1"></li>
+            </ol>
 
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox" style="border-bottom:solid 1px;">
+                <div class="item active">
+                    <img src="/Projet_BFCash/BFCash/Ressources/img/slide1.png" alt="slide1">
+                </div>
+
+                <div class="item">
+                    <img src="/Projet_BFCash/BFCash/Ressources/img/slide2.png" alt="slide2">
+                </div>
+
+            </div>
+
+        </div>
+        </p></br>
+        <div class="col-lg-4">
+            <img class="img-circle" src="/Projet_BFCash/BFCash/Ressources/img/icon-rapid.png" alt="icone2" width="140" height="140">
+            <h2>Rapide</h2>
+            <p>Envoi en quelques minutes</p>
+            <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+            <img class="img-circle" src="/Projet_BFCash/BFCash/Ressources/img/icon-securise.png" alt="icone2" width="140" height="140">
+            <h2>Sécurisé</h2>
+            <p>Processus de paiement sécurisé</p>
+            <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+            <img class="img-circle" src="/Projet_BFCash/BFCash/Ressources/img/icon-cout2.png" alt="icone3" width="140" height="140">
+            <h2>Moins cher</h2>
+            <p>Frais d'envoi à faible cout</p>
+            <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+        </div><!-- /.col-lg-4 -->
     </div>
     <div id="corps-right">
         </p>
